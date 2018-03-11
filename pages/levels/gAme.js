690a502327bf088a1348bb98229e60ba8fab8e5c
Jenna Kronenberg <jkronenberg2@hwemail.com>
2018-03-10 16:06:20 -0800

    hi

--- pages/levels/gAme.js
+++ pages/levels/gAme.js
@@ -1,15 +1,13 @@
-$(document).on('mousemove', function (e){
++$(document).on('mousemove', function (e){
     $('#X').html("<b>X:</b>" + " " + e.pageX);
     $('#Y').html("<b>Y:</b>" + " " + e.pageY);
     $('#coords').css({left:e.pageX, top:e.pageY});
 });
 var gameIsRunning = false;
 
-
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
-        
         minutes = parseInt(timer / 30, 10)
         seconds = parseInt(timer % 30, 10);
 
@@ -18,12 +16,11 @@ function startTimer(duration, display) {
 
         display.textContent =  "00:" + seconds;
 
-        if (seconds < 0) {
+        if (--timer < 0) {
             gameIsRunning = true;
             timer = duration;
         }
     }, 1000);
-    gameIsRunning = false;
 }
 
 window.onload = function () {
@@ -32,26 +29,28 @@ window.onload = function () {
         display = document.querySelector('#time');
     startTimer(minutes, display);
 };
-var total = new Date();
-var accuracy = 0;
+// var total = new Date();
+// var accuracy = 0;
 
-while (gameIsRunning == true){
+// while (gameIsRunning = true){
     
-    while (860>X>600 && 260<Y<400)
-    {
-        var sec = 0;
-        function pad ( val ) { return val > 9 ? val : "0" + val; }
-        setInterval( function(){
-            document.getElementById("seconds").innerHTML=pad(++sec%60);
-            document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-        }, 1000);
-        total.setSeconds(total + sec);
-    }
-}
+//     if (860>X>600 && 260<Y<400)
+//     {
+//         var sec = 0;
+//         function pad ( val ) { return val > 9 ? val : "0" + val; }
+//         setInterval( function(){
+//             document.getElementById("seconds").innerHTML=pad(++sec%60);
+//             document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+//         }, 1000);
+//         total.setSeconds(total + sec);
+//     }
+//     else 
+//     {
+//         total = total;
+//     }
+// }
 
-function calculateAccuracy()
-{
-    accuracy = total / 30 * 100;
-}
-
-function calulatePoint ()
+// function calculateAccuracy()
+// {
+//     accuracy = total / 30 * 100;
+// }
