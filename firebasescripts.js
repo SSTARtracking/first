e5871c9ef6d16b2095339ac736453e254f9f8951
Caroline Kaleel <cocokaleel@gmail.com>
2018-03-10 16:36:37 -0800

    Button Grayout works

--- firebasescripts.js
+++ firebasescripts.js
@@ -22,8 +22,11 @@ document.querySelector('#register').addEventListener('click',function (e){
         //log to the console that a user was created
         console.log("Successfully created user account: ", user.uid);
         //popup (toast) that the user account was created
-        Materialize.toast("Successfully created user account: " + user.uid, 4000);
-      })
+        Materialize.toast("Successfully created user account!", 4000);
+        $('#register').addClass('disabled');
+        $('#sign-in').addClass('disabled');
+        $('#sign-out').removeClass('disabled').addClass();
+    })
 
       //if it doesn't work
       .catch(function(error){
@@ -35,31 +38,32 @@ document.querySelector('#register').addEventListener('click',function (e){
         if (error.code === 'auth/email-already-in-use'){
           var credential = firebase.auth.EmailAuthProvider.credential (email, password);
         }
-        
       });
 })
 
 document.querySelector('#sign-in').addEventListener('click', function(e) {
-    Materialize.toast("1", 4000);
   
     e.preventDefault();
     e.stopPropagation();
-   
-    Materialize.toast("2", 4000);
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value
+    var credential = firebase.auth.EmailAuthProvider.credential(email, password);
+    
    
     var auth = firebase.auth();
     var currentUser = auth.currentUser;
     
-    Materialize.toast("3", 4000);
     
     firebase.auth().signInWithEmailAndPassword (email, password).then( function(user){    
-            Materialize.toast("4", 4000);
             
             console.log("Sign in successful", user);
             Materialize.toast("Sign in successful!", 4000);
 
             auth = user;
-        }).catch (function(error){  Materialize.toast("5", 4000);
+            $('#register').addClass('disabled');
+            $('#sign-in').addClass('disabled');
+            $('#sign-out').removeClass('disabled').addClass();
+        }).catch (function(error){
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log ('signIn error', error);
@@ -73,4 +77,7 @@ document.querySelector('#sign-out').addEventListener('click', function(e) {
     e.stopPropagation();
     firebase.auth().signOut();
     Materialize.toast("Signed Out", 4000);
+    $('#register').removeClass('disabled');
+    $('#sign-in').removeClass('disabled');
+    $('#sign-out').addClass('disabled');
 })
