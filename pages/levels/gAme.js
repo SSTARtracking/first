86cc673f9a9a6415c48892df6169077667f28122
Stephanie <ChoFamilia@Hannys-MacBook-Air.local>
2018-04-04 18:36:12 -0700

    Revert "yyayayayy"
    This reverts commit 7aeb172864e4b674065037d5a5366464eb57aa71.

--- pages/levels/gAme.js
+++ pages/levels/gAme.js
@@ -1,92 +1,76 @@
-<<<<<<< HEAD:levels/gAme.js
-(document).on('mousemove', function (e)
-{
-    ('#X').html("<b>X:</b>" + " " + e.pageX);
-    ('#Y').html("<b>Y:</b>" + " " + e.pageY);
-    ('#coords').css({left:e.pageX, top:e.pageY});
-=======
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-$(document).on('mousemove', function (e){
-    $('#X').html("<b>X:</b>" + " " + e.pageX);
-    $('#Y').html("<b>Y:</b>" + " " + e.pageY);
-    $('#coords').css({left:e.pageX, top:e.pageY});
-=======
->>>>>>> c6da126bb55932a8ece9c8775b38044f0689787c
+
 (document).on('mousemove', function (e)
 {
     ('#X').html("<b>X:</b>" + " " + e.pageX);
     ('#Y').html("<b>Y:</b>" + " " + e.pageY);
     ('#coords').css({left:e.pageX, top:e.pageY});
->>>>>>> 10b0084de68e029bc209f500d23ad997aceadaa5
->>>>>>> parent of 7aeb172... yyayayayy:pages/levels/gAme.js
+
 });
 var gameIsRunning = false;
 
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 30, 10)
-        seconds = parseInt(timer % 30, 10);
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 30, 10)
+        seconds = parseInt(timer % 30, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent =  "00:" + seconds;
+        display.textContent =  "00:" + seconds;
 
-        if (--timer < 0) {
-            // gameIsRunning = true;
-            timer = duration;
-        }
-    }, 1000);
+        if (--timer < 0) {
+            // gameIsRunning = true;
+            timer = duration;
+        }
+    }, 1000);
 }
 
 window.onload = function () {
 //this is where you can modifies the time amount.
-    var minutes= 30 ,
-        display = document.querySelector('#time');
-    startTimer(minutes, display);
-    
+    var minutes= 30 ,
+        display = document.querySelector('#time');
+    startTimer(minutes, display);
+    
 };
 
 
-function withinEyes ()  
+function withinEyes ()  
 {
-    if (860 > $('#X').html() > 600 && 260 < $('#Y').html() < 400)
-    {
+    if (860 > $('#X').html() > 600 && 260 < $('#Y').html() < 400)
+    {
 var sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    }, 1000);
-    }
-  
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
+    }
+  
 }
 // var total = new Date();
 // var accuracy = 0;
 
 // while (gameIsRunning = true){
-    
-//     if (860>X>600 && 260<Y<400)
-//     {
-//         var sec = 0;
-//         function pad ( val ) { return val > 9 ? val : "0" + val; }
-//         setInterval( function(){
-//             document.getElementById("seconds").innerHTML=pad(++sec%60);
-//             document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-//         }, 1000);
-//         total.setSeconds(total + sec);
-//     }
-//     else 
-//     {
-//         total = total;
-//     }
+    
+//     if (860>X>600 && 260<Y<400)
+//     {
+//         var sec = 0;
+//         function pad ( val ) { return val > 9 ? val : "0" + val; }
+//         setInterval( function(){
+//             document.getElementById("seconds").innerHTML=pad(++sec%60);
+//             document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+//         }, 1000);
+//         total.setSeconds(total + sec);
+//     }
+//     else 
+//     {
+//         total = total;
+//     }
 // }
 
 // function calculateAccuracy()
 // {
-//     accuracy = total / 30 * 100;
+//     accuracy = total / 30 * 100;
 // }
